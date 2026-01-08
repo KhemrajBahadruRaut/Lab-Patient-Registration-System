c2040e03409ef780deb56698e1aa718bf8762d15
KhemrajBahadurRaut <amanraut76@gmail.com>
2026-01-08 14:18:58 +0545

    investigation section

--- src/components/Forms/InvestigationSelector.jsx
+++ src/components/Forms/InvestigationSelector.jsx
@@ -70,7 +70,7 @@ const InvestigationSelector = ({ selectedTests, onTestsChange }) => {
                         }}
                         onFocus={() => setShowDropdown(true)}
                     />
-                    {showDropdown && searchTerm && filteredTests.length > 0 && (
+                    {showDropdown && filteredTests.length > 0 && (
                         <div className="absolute z-10 w-full mt-1 bg-white border border-gray-300 rounded-md shadow-lg max-h-60 overflow-y-auto">
                             {filteredTests.map(test => (
                                 <div
